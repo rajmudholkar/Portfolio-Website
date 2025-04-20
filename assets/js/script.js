b22d449f565316e2b5e37d421a831ef5792beda3
rajmudholkar <raju.mudholkar1313@gmail.com>
2025-04-20 10:24:45 +0530

    updated mobile app devlelopmemnt

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "angular development", "web development"],
+    strings: ["frontend development", "angular development", "Mobile App development", "web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
